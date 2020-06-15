da5063ec680b5748a054167a64438711b849b025
Aman Bahuguna <65839144+amanbh123@users.noreply.github.com>
2020-06-15 21:59:57 +0530

    1

---
+++ 15 June/VowelPhobia.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        string s;
+        cin >> s;
+        int count = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') {
+                count++;
+            }
+        }
+        cout << count << endl;
+    }
+}
